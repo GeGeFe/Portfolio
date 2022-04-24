8a22d3699b4e67303418e63ce79b433734529961
GeGeFe <gabrielgf2002@yahoo.com.ar>
2022-04-24 13:19:20 -0300

    Separando material en una clase aparte.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,14 +18,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { SesionComponent } from './sesion/sesion.component';
 import { AutenticacionService } from './servicios/autenticacion.service';
 import { interceptorProvider } from './sesion/interceptor.service';
+import { MaterialModule } from './material.module';
 
-// Material imports
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
@@ -43,15 +38,10 @@ import { MatSelectModule } from '@angular/material/select';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatGridListModule,
-    MatCardModule,
     ReactiveFormsModule,
     FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [BaseDeDatosService, AutenticacionService, interceptorProvider],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,43 @@
+/*
+import {MatNativeDateModule,MatSnackBarModule,MatIconModule,MatDialogModule, MatButtonModule, MatTableModule, MatPaginatorModule , MatSortModule,MatTabsModule, MatCheckboxModule, MatToolbarModule, MatCard, MatCardModule, MatFormField, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule } from  '@angular/material';
+import {MatDatepickerModule} from  '@angular/material/datepicker';
+import {MatRadioModule} from  '@angular/material/radio';
+import {MatSelectModule} from  '@angular/material/select';
+import {MatSliderModule} from  '@angular/material/slider';
+import {MatDividerModule} from  '@angular/material/divider';
+
+@NgModule({
+imports: [MatTabsModule,MatDividerModule,MatSliderModule,MatSelectModule,MatRadioModule,MatNativeDateModule,MatDatepickerModule,MatSnackBarModule,MatIconModule,MatDialogModule,MatProgressSpinnerModule,MatButtonModule,MatSortModule,MatTableModule,MatTabsModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule, MatPaginatorModule],
+exports: [MatTabsModule,MatDividerModule,MatSliderModule,MatSelectModule,MatRadioModule,MatNativeDateModule,MatDatepickerModule,MatSnackBarModule,MatIconModule,MatDialogModule,MatProgressSpinnerModule,MatButtonModule,MatSortModule, MatCheckboxModule, MatToolbarModule, MatCardModule,MatTableModule,MatTabsModule, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule, MatPaginatorModule],
+
+})
+
+export  class  MyMaterialModule { }
+*/
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [
+        MatGridListModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatSelectModule
+    ],
+    exports: [
+        MatGridListModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatSelectModule
+    ]
+})
+export class MaterialModule { };
